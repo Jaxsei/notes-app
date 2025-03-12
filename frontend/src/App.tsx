fa50484a11fd44ca8c0f5e6e24baf6971bdb4c50
jaxsei <thakuriamohan18@gmail.com>
2025-03-12 20:54:49 +0530

    Reinitialize Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl bg-sky-600 text-white p-4 hover:bg-sky-400 text-center duration-300 rounded-md">NotesApp | Jaxsei</h1>
+    </>
+  )
+}
+
+export default App
