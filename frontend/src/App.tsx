c27c038b417db5863e12fa2ce7190cfb71b4d698
jaxsei <thakuriamohan18@gmail.com>
2025-03-18 11:47:43 +0530

    Reinitialize frontend and Add Shadcn/ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-function App() {
+import React from 'react'
 
+function App() {
   return (
     <>
-      <h1 className="text-3xl bg-sky-600 text-white p-4 hover:bg-sky-400 text-center duration-300 rounded-md">NotesApp | Jaxsei</h1>
     </>
   )
 }
