7118906b45f4824aeb601e131639a35e3b4fa08b
Jaxsei <150438473+Jaxsei@users.noreply.github.com>
2025-05-31 14:43:50 +0530

    Add trust proxy

--- backend/app.ts
+++ backend/app.ts
@@ -15,6 +15,7 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));   // For JSON data
 app.use(express.urlencoded({ extended: true, limit: '50mb' })); // For form data
 app.use(cookieParser());
+app.set("trust proxy", 1);
 
 
 // Routes declaration
