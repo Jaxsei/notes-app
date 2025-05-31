84bf31bacc37268f4c4116909d9c6872c544d996
Jaxsei <thakuriamohan18@gmail.com>
2025-05-31 13:48:56 +0530

    Fix: bugs in deployment #13

--- frontend/src/components/utils/axios.ts
+++ frontend/src/components/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000/api/v1" : "https://notes-app-3nbt.onrender.com",
+  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000/api/v1" : "https://notes-app-3nbt.onrender.com/api/v1",
   withCredentials: true,
 });
