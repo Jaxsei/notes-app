3cd57d2912e122a8b2354e267fdf53163d9b6461
Jaxsei <thakuriamohan18@gmail.com>
2025-06-01 17:18:16 +0530

    Fix: changes in package.json #11

--- backend/app.ts
+++ backend/app.ts
@@ -16,7 +16,7 @@ const app = express();
 app.set("trust proxy", 1);
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Change this to your deployed frontend URL in production
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 
@@ -28,14 +28,16 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/notes", noteRoutes);
 
-
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const rootPath = path.join(__dirname, "../../");
+
+  app.use(express.static(path.join(rootPath, "frontend", "dist")));
 
   app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(rootPath, "frontend", "dist", "index.html"));
   });
 }
+
 export { app };
