df15b3f84fb0da05f2d31fa9ef8074e5e7d1a40e
Jaxsei <thakuriamohan18@gmail.com>
2025-06-01 21:18:32 +0530

    Feat. uhh uhh #1

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ const app = express();
 // Middleware
 app.set("trust proxy", 1);
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
+  origin: 'http://localhost:5173',
+  credentials: true,
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -27,11 +27,13 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/notes", noteRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    const __dirnamw = path.resolve(); // This points to project root when run from base dir
-    app.use(express.static(path.join(__dirnamw, "frontend", "dist")));
-    app.get(/^\/(?!api).*/, (req, res) => {
-        res.sendFile(path.join(__dirnamw, "frontend", "dist", "index.html"));
-    });
+  const __dirname = path.resolve(path.dirname(new URL(import.meta.url).pathname));
+
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
 export { app };
