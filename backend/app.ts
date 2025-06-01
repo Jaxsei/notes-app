69aff84e89fcd35432501b3bbddce1104fc5c7f8
Jaxsei <thakuriamohan18@gmail.com>
2025-06-01 16:58:08 +0530

    Fix: changes in package.json #8

--- backend/app.ts
+++ backend/app.ts
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(path.join(rootPath, "frontend", "dist")));
 
-  app.get("/*splat", (_req, res) => {
+  app.get(/^\/(?!api).*/, (_req, res) => {
     res.sendFile(path.join(rootPath, "frontend", "dist", "index.html"));
   });
 }
