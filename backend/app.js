b8a2f03a385ad45f7bcda5575235c520b579e8bf
Jaxsei <150438473+Jaxsei@users.noreply.github.com>
2026-03-23 22:59:02 +0530

    Fix helmet CSP bug

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,16 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "img-src": ["'self'", "data:", "https://res.cloudinary.com"],
+      },
+    },
+  })
+);
 app.use(morgan("combined"));
 
 // API Routes
