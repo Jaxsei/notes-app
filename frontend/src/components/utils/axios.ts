a96c85b46fc4ca881894138f33d341737580ac29
Jaxsei <thakuriamohan18@gmail.com>
2026-03-29 14:55:59 +0530

    Fix Syntax error

--- frontend/src/components/utils/axios.ts
+++ frontend/src/components/utils/axios.ts
@@ -4,10 +4,10 @@ const devUrl = "http://localhost:5000/api/v1";
 const prodUrl = "/api/v1";
 
 export const axiosInstance = axios.create({
-  baseURL: devUrl,
-  import.meta.env.MODE === "development"
-   ? "http://localhost:5000/api/v1"
-  : "/api/v1"
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api/v1"
+      : "/api/v1",
   withCredentials: true,
 });
 
