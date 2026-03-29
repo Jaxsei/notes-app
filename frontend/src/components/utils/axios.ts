f874300a351be173dbada4cdb2f2146427bf0005
Jaxsei <150438473+Jaxsei@users.noreply.github.com>
2026-03-29 14:52:14 +0530

    Update axios.ts

--- frontend/src/components/utils/axios.ts
+++ frontend/src/components/utils/axios.ts
@@ -5,9 +5,9 @@ const prodUrl = "/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL: devUrl,
-  //import.meta.env.MODE === "development"
-  // ? "http://localhost:5000/api/v1"
-  // : "/api/v1"
+  import.meta.env.MODE === "development"
+   ? "http://localhost:5000/api/v1"
+  : "/api/v1"
   withCredentials: true,
 });
 
