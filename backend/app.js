e93527235678f7d2a7552498bfe6c0603054706a
Jaxsei <150438473+Jaxsei@users.noreply.github.com>
2026-03-30 00:06:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,6 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(helmet());
 app.use(
   helmet({
     contentSecurityPolicy: {
